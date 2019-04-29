247d3b3cd8fc7d13e072aeb1166304a89be99a7c
Will Blackburn <harkole@gmail.com>
2019-04-29 21:34:26 +0100

    minor work to UI layout

--- src/Pages/NewPost/NewPost.tsx
+++ src/Pages/NewPost/NewPost.tsx
@@ -2,31 +2,53 @@ import React from 'react';
 
 import styles from './NewPost.module.css';
 
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
+const TextField = React.lazy(() => import('@material-ui/core/TextField'));
+const Paper = React.lazy(() => import('@material-ui/core/Paper'));
+const Grid = React.lazy(() => import('@material-ui/core/Grid'));
 
 const NewPost = () => {
 
     return (
         <Paper className={styles.container} elevation={8}>
-            <TextField
-                id='title'
-                name='title'
-                label='Title'
-                fullWidth
-                required
-            />
-            <TextField
-                id='subtitle'
-                name='subtitle'
-                label='Subtitle'
-            />
-            <TextField
-                id='thought'
-                name='thought'
-                label='Thought'
-            />
-            
+            <Grid container className={styles.root}>
+                <Grid item xs={12} className={styles.space}>
+                    <TextField
+                        id='title'
+                        name='title'
+                        label='Title'
+                        fullWidth
+                        required
+                    />
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item xs={6} className={styles.space}>
+                    <TextField
+                        id='subtitle'
+                        name='subtitle'
+                        label='Subtitle'
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={6} className={styles.space}>
+                    <TextField
+                        id='thought'
+                        name='thought'
+                        label='Thought'
+                        fullWidth
+                    />
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item xs={12} className={styles.space}>
+                    <TextField
+                        id='description'
+                        name='description'
+                        label='Description'
+                        fullWidth
+                    />
+                </Grid>
+            </Grid>
         </Paper>
     );
 }
